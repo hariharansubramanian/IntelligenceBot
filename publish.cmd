18b8b445a99db663d76c50071fcb072c4a565b99
Hariharan Subramanian <hariharan.subramanian@ivanti.com>
2017-12-21 15:22:24 +0530

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=tbdbotmenlp-Web-Deploy.pubxml -p:Password=rxR5wm8qaiXjratAFpgf8dQtfQRvESQ2xS4z8tBWZJszchQAL6e1jRtAmidQ
+
