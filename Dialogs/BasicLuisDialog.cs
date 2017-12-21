1f9f9f03a1919c3deff156bd0bb6a3000031e680
Hariharan Subramanian <hariharan.subramanian@ivanti.com>
2017-12-21 19:59:33 +0530

    Creating new incident successfully!

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -1,10 +1,11 @@
-using System;
-using System.Configuration;
-using System.Threading.Tasks;
-
+using LuisBot.Models;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
 
 namespace Microsoft.Bot.Sample.LuisBot
 {
@@ -12,12 +13,16 @@ namespace Microsoft.Bot.Sample.LuisBot
     [Serializable]
     public class BasicLuisDialog : LuisDialog<object>
     {
-        
+        HashSet<Incident> incidents;
         public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(
-            ConfigurationManager.AppSettings["LuisAppId"], 
-            ConfigurationManager.AppSettings["LuisAPIKey"], 
+            ConfigurationManager.AppSettings["LuisAppId"],
+            ConfigurationManager.AppSettings["LuisAPIKey"],
             domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
         {
+            if (incidents == null)
+            {
+                incidents = new HashSet<Incident>();
+            }
         }
 
         [LuisIntent("create")]
@@ -25,13 +30,13 @@ namespace Microsoft.Bot.Sample.LuisBot
         {
             await this.manageCreateIncident(context, result);
         }
-        
+
         [LuisIntent("None")]
         public async Task NoneIntent(IDialogContext context, LuisResult result)
         {
             await this.ShowLuisResult(context, result);
         }
-      
+
         // Go to https://luis.ai and create a new intent, then train/publish your luis app.
         // Finally replace "Gretting" with the name of your newly created intent in the following handler
         [LuisIntent("Greeting")]
@@ -52,15 +57,22 @@ namespace Microsoft.Bot.Sample.LuisBot
             await this.ShowLuisResult(context, result);
         }
 
-        private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
+        private async Task ShowLuisResult(IDialogContext context, LuisResult result)
         {
             await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
             context.Wait(MessageReceived);
         }
-        
-        private async Task manageCreateIncident(IDialogContext context, LuisResult result) 
+
+        private async Task manageCreateIncident(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"Sure. Could you please elaborate on the issue as: description so and so ");
+            Incident incident = new Incident();
+            incident.id = System.Guid.NewGuid().ToString();
+            incident.owner = "Hari";
+            incident.summary = result.Query;
+            incident.status = "New";
+            string str = String.Format("Incident \"{0}\" created by {1} successfuly!\n Incident ID: {2}", incident.summary, incident.owner, incident.id);
+            incidents.Add(incident);
+            await context.PostAsync(str);
             context.Wait(MessageReceived);
         }
     }
---
+++ Models/Incident.cs
@@ -0,0 +1,13 @@
+﻿namespace LuisBot.Models
+{
+    public class Incident
+    {
+
+        public string id { get; set; }
+        public string summary { get; set; }
+        public string priority { get; set; }
+        public string owner { get; set; }
+        public string status { get; set; }
+
+    }
+}
